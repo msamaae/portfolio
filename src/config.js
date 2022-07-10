391e0bb67ee73162c814b72971540740f80a90e9
ColaBebsi <m.samaae@hotmail.com>
2022-07-10 22:03:16 +0200

    yey

--- src/config.js
+++ src/config.js
@@ -1,27 +1,27 @@
 module.exports = {
-  email: 'brittany.chiang@gmail.com',
+  email: 'msammae@hotmail.com',
 
   socialMedia: [
     {
       name: 'GitHub',
       url: 'https://github.com/bchiang7',
     },
-    {
-      name: 'Instagram',
-      url: 'https://www.instagram.com/bchiang7',
-    },
-    {
-      name: 'Twitter',
-      url: 'https://twitter.com/bchiang7',
-    },
+    // {
+    //   name: 'Instagram',
+    //   url: 'https://www.instagram.com/bchiang7',
+    // },
+    // {
+    //   name: 'Twitter',
+    //   url: 'https://twitter.com/bchiang7',
+    // },
     {
       name: 'Linkedin',
       url: 'https://www.linkedin.com/in/bchiang7',
     },
-    {
-      name: 'Codepen',
-      url: 'https://codepen.io/bchiang7',
-    },
+    // {
+    //   name: 'Codepen',
+    //   url: 'https://codepen.io/bchiang7',
+    // },
   ],
 
   navLinks: [
