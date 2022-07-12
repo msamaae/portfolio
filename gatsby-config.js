48e0cac976447c46a414d44f19e1ba5ef560e592
ColaBebsi <m.samaae@hotmail.com>
2022-07-12 23:50:09 +0200

    yey

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Moohammad Samaae',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Moohammad Samaae is a software developer who specializes in building (and occasionally designing) exceptional digital experiences.',
+    siteUrl: 'https://msamaae.netlify.app', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@msamaae',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
